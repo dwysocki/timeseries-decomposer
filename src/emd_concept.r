c7db6467e3291cbac85e20aaddd78c3a455e7d7e
Daniel Wysocki <dwysocki@oswego.edu>
2016-01-18 04:54:59 -0500

    Added basic determinism extraction criterion and improved FFT

--- src/emd_concept.r
+++ src/emd_concept.r
@@ -16,7 +16,7 @@ sinusoid <- function(t, period = 1.0) {
 
 sawtooth <- function(t, period = 1.0) {
 
-  t.over.period = t / period
+  t.over.period <- t / period
 
   2 * (t.over.period - floor(0.5 + t.over.period))
 
@@ -58,21 +58,57 @@ generate.signal <- function(fn = sinusoid,
 }
 
 
-separate.signal <- function(t, x) {
+decompose.signal <- function(t, x) {
 
-  result <- emd(xt = x, tt = t, boundary = "periodic")
+  emd(xt = x, tt = t, boundary = "periodic")
 
 }
 
 
-phase.spectra <- function(imfs) {
+fourier.transform <- function(t, imfs) {
 
   ffts <- apply(imfs, 2, fft)
+  amplitudes <- apply(ffts, 2, function(x) { Mod(x)^2 })
   phases <- apply(ffts, 2, Arg)
 
-  list(fft           = ffts,
+  N <- length(t)
+  T <- max(t) - min(t)
+  delta.f <- 1/T
+  f.max <- (N/2) * delta.f
+  freq <- seq(-f.max, f.max, delta.f)[-floor(N/2)]
+
+  list(freq          = freq,
+       fft           = ffts,
+       amplitudes    = amplitudes,
        phase.spectra = phases,
        nimf          = dim(imfs)[2])
+
+}
+
+
+extract.determinism <- function(emd.result, fourier.transform,
+                                criteria = 2) {
+
+  # placeholder, in the future this will only happen if criteria is an
+  # integer, and otherwise will assume it's a function which returns an
+  # integer
+  index <- criteria
+
+  components <- emd.result$imf[,(index:emd.result$nimf)]
+
+  rowSums(components)
+
+}
+
+
+fit.statistics <- function(signal, deterministic.signal) {
+
+  N <- length(deterministic.signal)
+  RSS <- sum((signal$signal - deterministic.signal)^2)
+
+  MSE <- RSS / N
+
+  data.frame(MSE = MSE)
 
 }
 
@@ -85,6 +121,20 @@ plot.signal <- function(signal) {
        type = "p", lwd = 1, pch = ".",
        xlab = "t", ylab = "x(t)")
   lines(signal$t, signal$signal,
+        col = "red")
+
+  dev.off()
+
+}
+
+
+plot.determinism <- function(signal, deterministic.signal) {
+
+  pdf("deterministic_signal.pdf")
+
+  plot(signal$t, signal$signal,
+       type = "l", col = "blue")
+  lines(signal$t, deterministic.signal,
         col = "red")
 
   dev.off()
@@ -118,17 +168,38 @@ plot.emd <- function(signal, emd.result) {
 }
 
 
-plot.phase.spectra <- function(signal, phase.spectra) {
+plot.amplitude.spectra <- function(fourier.transform) {
+
+  pdf("imf_amplitude_spectra.pdf")
+
+  par(mfrow = c(ceiling(fourier.transform$nimf / 2), 2))
+
+  N <- length(fourier.transform$freq)
+  idx <- seq(floor(N/2), N)
+
+  for(i in 1:fourier.transform$nimf) {
+
+    amplitudes <- fourier.transform$amplitudes[idx,i]
+    plot(fourier.transform$freq[idx], amplitudes,
+         type = "l")
+
+  }
+
+  dev.off()
+
+}
+
+
+plot.phase.spectra <- function(fourier.transform) {
 
   pdf("imf_phase_spectra.pdf")
 
-  par(mfrow = c(ceiling(phase.spectra$nimf / 2), 2))
+  par(mfrow = c(ceiling(fourier.transform$nimf / 2), 2))
 
-  for(i in 1:phase.spectra$nimf) {
+  for(i in 1:fourier.transform$nimf) {
 
-    phase.spectrum <- phase.spectra$phase.spectra[,i]
-    print(phase.spectra$phase.spectra)
-    plot(signal$t, phase.spectrum,
+    phase.spectrum <- fourier.transform$phase.spectra[,i]
+    plot(fourier.transform$freq, phase.spectrum,
          type = "l")
 
   }
@@ -140,7 +211,7 @@ plot.phase.spectra <- function(signal, phase.spectra) {
 
 plot.imf.partitions <- function(signal, emd.result) {
 
-  pdf("extracted_determinism_guesses.pdf")
+  pdf("extracted_determinism_candidates.pdf")
 
   par(mfrow = c(ceiling(emd.result$nimf/2), 2))
 
@@ -171,16 +242,23 @@ main <- function() {
 
   set.seed(4)
 
-  signal <- generate.signal(square,
+  signal <- generate.signal(sinusoid,
                             noise.sd = 0.5, num.cycles = 5)
 
-  emd.result <- separate.signal(signal$t, signal$observation)
-  phase.spectra <- phase.spectra(emd.result$imf)
+  emd.result <- decompose.signal(signal$t, signal$observation)
+  fourier.transform <- fourier.transform(signal$t, emd.result$imf)
+  deterministic.signal <- extract.determinism(emd.result, fourier.transform,
+                                              criteria = 3)
+  stats <- fit.statistics(signal, deterministic.signal)
 
   plot.signal(signal)
+  plot.determinism(signal, deterministic.signal)
   plot.emd(signal, emd.result)
-  plot.phase.spectra(signal, phase.spectra)
+  plot.amplitude.spectra(fourier.transform)
+  plot.phase.spectra(fourier.transform)
   plot.imf.partitions(signal, emd.result)
+
+  print(stats, row.names = FALSE)
 
 }
 
